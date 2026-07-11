c7a0a8cc0c95e89abb57277f48a99843c0172eab
shhahidd <patelshahid260@gmail.com>
2026-07-11 17:46:30 +0530

    fix: remove unused Link import in DetailsPage

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams, Link } from "react-router";
+import { useLocation, useParams } from "react-router";
 import {
   Button,
   Card,
